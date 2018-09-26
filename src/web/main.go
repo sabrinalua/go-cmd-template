682adf946d83f8795e319068e462c5d731fdf2c3
root <sabrinalua@users.noreply.github.com>
2018-09-27 03:56:26 +0800

    2nd commit

--- src/web/main.go
+++ src/web/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
   "fmt"
+  "os"
+  flag "github.com/ogier/pflag"
+)
+var (
+	input Input
 )
 func main()  {
-  fmt.Printf("main\n")
+  flag.Parse()
+  if flag.NFlag() == 0 {
+     fmt.Printf("Usage: %s [options]\n", os.Args[0])
+     fmt.Println("Options:")
+     flag.PrintDefaults()
+     os.Exit(1)
+  }
+  if input.Name!=""{
+  	fmt.Printf("Hello, %s!\n", input.Name)
+  }
 }
 
 func init()  {
-
+	flag.StringVarP(&input.Name, "name", "n", "", "Enter your name")
 }
